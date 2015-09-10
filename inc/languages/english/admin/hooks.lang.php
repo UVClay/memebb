cddee9dd84abcf102a83ae90b84b0ee7799f6964
UVClay <clayallen3@gmail.com>
2015-09-10 10:55:33 -0400

    adding patches mod temporarily.  will roll back when i merge prior patches

---
+++ inc/languages/english/admin/hooks.lang.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This file is part of Hooks plugin for MyBB.
+ * Copyright (C) 2011 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+$l = array(
+    'hooks' => 'Hooks',
+    'hooks_PL' => 'The Hooks plugin depends on <a href="http://mods.mybb.com/view/pluginlibrary">PluginLibrary</a>, which is missing. Please install it.',
+    'hooks_PL_old' => 'The Hooks plugin depends on <a href="http://mods.mybb.com/view/pluginlibrary">PluginLibrary</a>, which is too old. Please update it.',
+    'hooks_activate' => 'Activate',
+    'hooks_activated' => 'The selected hook has been activated.',
+    'hooks_argument' => 'Argument',
+    'hooks_argument_desc' => "If this hook takes an argument / parameter, enter the desired variable name here. For example, if you set this to 'arg', you can use $arg in your code.",
+    'hooks_cancel' => 'Cancel',
+    'hooks_code' => 'Code',
+    'hooks_code_desc' => 'Enter the PHP code that should be executed for this hook.',
+    'hooks_controls' => 'Controls',
+    'hooks_deactivate' => 'Deactivate',
+    'hooks_deactivated' => 'The selected hook has been deactivated.',
+    'hooks_delete' => 'Delete Hook',
+    'hooks_deleted' => 'The selected hook has been deleted.',
+    'hooks_desc' => 'Create and manage plugin hooks. Depends on PluginLibrary.',
+    'hooks_description' => 'Description',
+    'hooks_description_desc' => 'Optionally, you may enter a description for this hook.',
+    'hooks_edit' => 'Edit Hook',
+    'hooks_error_argument' => 'Invalid argument name.',
+    'hooks_error_code' => 'No code specified.',
+    'hooks_error_faulty' => 'Could not activate the selected hook because it contains errors.',
+    'hooks_error_hook' => 'No hook specified.',
+    'hooks_error_hook_invalid' => 'Invalid hook specified.',
+    'hooks_error_key' => 'Invalid post key.',
+    'hooks_error_missing' => 'The specified hook does not exist.',
+    'hooks_error_syntax' => 'There is a syntax error in your code.',
+    'hooks_error_title' => 'No title specified.',
+    'hooks_error_write_permission' => "Write permission for '{1}' is missing.",
+    'hooks_export' => 'Export',
+    'hooks_export_button' => 'Export as XML',
+    'hooks_export_caption' => 'Export Hooks',
+    'hooks_export_error' => 'Failed to export hooks. None selected?',
+    'hooks_export_error_unknown' => 'Failed to export XML - please report a bug.',
+    'hooks_export_filename' => 'Enter a name component for the exported file. The file will be called <i>hooks-<b>name</b>.xml</i> or <b>name</b>.php.',
+    'hooks_export_plugin_author' => 'The name of the author of the plugin.',
+    'hooks_export_plugin_authorsite' => '(Optional) The URL to the website of the author.',
+    'hooks_export_plugin_button' => 'Export as Plugin',
+    'hooks_export_plugin_caption' => 'Export Hooks as Plugin Information',
+    'hooks_export_plugin_compatibility' => 'A CSV list of MyBB versions supported. E.g. 16* or 1604,1605,1606. Wildcards supported.',
+    'hooks_export_plugin_description' => '(Optional) The description of what the plugin does.',
+    'hooks_export_plugin_error_author' => 'No plugin author specified.',
+    'hooks_export_plugin_error_compatibility' => 'No plugin compatibility specified.',
+    'hooks_export_plugin_error_filename' => 'No plugin filename specified.',
+    'hooks_export_plugin_error_name' => 'No plugin name specified.',
+    'hooks_export_plugin_error_prefix' => 'Invalid plugin filename. Use a-z_ only for plugin export.',
+    'hooks_export_plugin_error_version' => 'No plugin version specified.',
+    'hooks_export_plugin_guid' => '(Optional) Unique GUID issued by the MyBB Mods site for version checking.',
+    'hooks_export_plugin_name' => 'The name of the plugin.',
+    'hooks_export_plugin_version' => 'The version number of the plugin.',
+    'hooks_export_plugin_website' => '(Optional) The website the plugin is maintained at.',
+    'hooks_export_select' => 'Select the hooks you want to export here. By default, the currently active hooks are selected. Hold down CTRL to select multiple hooks. Hook names are shown for your convenience, selecting them does nothing.',
+    'hooks_hook' => 'Hook',
+    'hooks_hook_desc' => 'Enter the name of the hook.',
+    'hooks_import' => 'Import',
+    'hooks_import_badfile' => 'Failed to import hooks. The file contained errors.',
+    'hooks_import_button' => 'Perform Import',
+    'hooks_import_caption' => 'Import Hooks',
+    'hooks_import_errors' => ' (some hooks may be missing due to {1} errors in the file)',
+    'hooks_import_file' => 'Select a hooks XML file to import.',
+    'hooks_import_nofile' => 'Failed to import hooks. Apparently no file was uploaded.',
+    'hooks_import_success' => '{1} Hooks successfully imported.',
+    'hooks_legend_cross' => 'Hook is disabled and inactive',
+    'hooks_legend_tick' => 'Hook is enabled and active',
+    'hooks_new' => 'Add a new Hook...',
+    'hooks_plugin_uninstall' => "Are you sure you want to uninstall Hooks? This will remove all your Hooks from the database.",
+    'hooks_plugin_uninstall_confirm' => "Click here to uninstall Hooks.",
+    'hooks_preview' => 'Preview Hook',
+    'hooks_preview_output' => 'Preview',
+    'hooks_priority' => 'Priority',
+    'hooks_priority_desc' => 'Specify the priority of this hook. Default value is 10. Lower value means higher priority.',
+    'hooks_save' => 'Save Hook',
+    'hooks_saved' => 'Hook saved.',
+    'hooks_tab_desc' => 'This section allows you to create and manage plugin hooks.',
+    'hooks_title' => 'Title',
+    'hooks_title_desc' => 'Enter a title for this hook.',
+    );
+
+?>
---
+++ inc/languages/english/admin/patches.lang.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * This file is part of Patches plugin for MyBB.
+ * Copyright (C) 2011 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+$l = array(
+    'patches' => 'Patches',
+    'patches_PL' => 'The Patches plugin depends on <a href="http://mods.mybb.com/view/pluginlibrary">PluginLibrary</a>, which is missing. Please install it.',
+    'patches_PL_old' => 'The Patches plugin depends on <a href="http://mods.mybb.com/view/pluginlibrary">PluginLibrary</a>, which is too old. Please update it.',
+    'patches_activate' => 'Activate',
+    'patches_activated' => 'The selected Patch was activated successfully.',
+    'patches_after' => 'Insert After',
+    'patches_after_desc' => 'If you want to insert new code after the match, enter it here.',
+    'patches_applied' => 'The file was modified successfully.',
+    'patches_apply' => 'Apply',
+    'patches_before' => 'Insert Before',
+    'patches_before_desc' => 'If you want to insert new code before the match, enter it here.',
+    'patches_cancel' => 'Cancel',
+    'patches_controls' => 'Controls',
+    'patches_cross' => 'FAIL',
+    'patches_deactivate' => 'Deactivate',
+    'patches_deactivated' => 'The selected Patch was deactivated successfully.',
+    'patches_debug' => 'Failed to apply edits. Debug info below.',
+    'patches_delete' => 'Delete Patch',
+    'patches_deleted' => 'The selected Patch has been deleted.',
+    'patches_desc' => 'Manage code modifications to MyBB core files. Depends on PluginLibrary.',
+    'patches_description' => 'Description',
+    'patches_description_desc' => '(Optional) Enter a description for this patch. It will be displayed in the list of patches.',
+    'patches_edit' => 'Edit Patch',
+    'patches_error' => 'No patch specified.',
+    'patches_error_edit' => 'No edit specified.',
+    'patches_error_file' => 'File does not exist.',
+    'patches_error_key' => 'Invalid post key.',
+    'patches_error_search' => 'Empty search pattern.',
+    'patches_error_title' => 'No title specified.',
+    'patches_error_write' => 'The file is not writable.',
+    'patches_export' => 'Export',
+    'patches_export_button' => 'Perform Export',
+    'patches_export_caption' => 'Export Patches',
+    'patches_export_error' => 'Failed to export patches. None selected?',
+    'patches_export_filename' => '(Optional) Enter a name component for the exported file. The file will be called <i>patches-<b>name</b>.xml</i>.',
+    'patches_export_select' => 'Select the patches you want to export here. By default, the currently active patches are selected. Hold down CTRL to select multiple patches. Filenames are shown for your convenience, selecting them does nothing.',
+    'patches_filename' => 'Filename',
+    'patches_filename_desc' => 'Enter the name of the file you want to modify.',
+    'patches_import' => 'Import',
+    'patches_import_badfile' => 'Failed to import patches. The file contained errors.',
+    'patches_import_button' => 'Perform Import',
+    'patches_import_caption' => 'Import Patches',
+    'patches_import_errors' => ' (some patches may be missing due to {1} errors in the file)',
+    'patches_import_file' => 'Select a patches XML file to import.',
+    'patches_import_nofile' => 'Failed to import patches. Apparently no file was uploaded.',
+    'patches_import_success' => '{1} Patches successfully imported.',
+    'patches_legend_cross' => 'Patch is activated but not applied, or the file was modified',
+    'patches_legend_nochange' => 'Patch is deactivated and reverted',
+    'patches_legend_tick' => 'Patch is activated and applied',
+    'patches_legend_warning' => 'Patch is deactivated but not reverted',
+    'patches_multi' => 'Multiple Matches',
+    'patches_multi_desc' => '(Advanced) Allow the search pattern to match more than once. By default, it has to be a unique match. If unsure, say NO.',
+    'patches_new' => 'Add a new Patch...',
+    'patches_nochange' => '-',
+    'patches_none' => 'Zero Matches',
+    'patches_none_desc' => '(Advanced) Allow the search pattern to have no matches at all. By default, the match is mandatory. If unsure, say NO.',
+    'patches_patch' => 'Patch',
+    'patches_plugin_uninstall' => "Are you sure you want to uninstall Patches? This will remove your Patches database. You won't be able to apply/revert Patches anymore.",
+    'patches_plugin_uninstall_confirm' => "Click here to uninstall Patches.",
+    'patches_preview' => 'Preview',
+    'patches_preview_active' => 'Preview active patches',
+    'patches_preview_file' => 'Preview active patches for {1}',
+    'patches_replace' => 'Replace',
+    'patches_replace_desc' => 'If you want to replace or remove the matched code, say YES, otherwise NO.',
+    'patches_revert' => 'Revert',
+    'patches_save' => 'Save Patch',
+    'patches_saved' => 'Patch saved.',
+    'patches_search' => 'Search Pattern',
+    'patches_search_desc' => 'Enter the search pattern, for example the code snippet you want to modify. One condition per line. There are no special characters, instead each condition is matched literally and case-sensitive. The search pattern will match the smallest group of lines in a file that contain all conditions in the correct order. The pattern has to be unique (only one match in the whole file). For details, refer to the PluginLibrary documentation on Search Patterns.',
+    'patches_tab_desc' => 'This section allows you to manage code modifications to MyBB core files.',
+    'patches_tick' => 'OK',
+    'patches_title' => 'Title',
+    'patches_title_desc' => 'Enter a title for this patch. It will be displayed in the list of patches.',
+    'patches_warning' => 'WARNING',
+    );
+
+?>
